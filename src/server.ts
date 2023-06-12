5cbe722a21ffbd64362b97dc7e3e7f42f638d3cd
hasanulhasan <hasanlink71@yahoo.com>
2023-06-13 00:09:54 +0600

    lint-husky-stage implemented

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,19 @@
-const mongoose = require('mongoose');
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string)
 
     console.log('database connceted successfully')
 
     app.listen(config.port, () => {
       console.log(`Application listening on port ${config.port}`)
     })
-
   } catch (error) {
     console.log('fail to connect', error)
   }
 }
 
-
-bootstrap();
+bootstrap()
